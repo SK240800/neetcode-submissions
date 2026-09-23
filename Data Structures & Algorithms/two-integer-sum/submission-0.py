5d489fa04f85e778382a3e65a718084236a698ac
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2026-09-22 19:14:13 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            d=target-nums[i]
+            if d in map:
+                return[map[d],i]
+            map[nums[i]]=i
+
+        
